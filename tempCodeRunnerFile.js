57730dc4367ca17a66eba0876194e1ddf59d8086
Shital K. Ghatole <88610195+Alpharox001@users.noreply.github.com>
2022-10-14 18:45:28 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let randomNumber = Math.floor(Math.random() * 6 + 1);
-
-// let pickRandomImage = images[randomNumber];
-
-// document.querySelectorAll(".dice img1")[0].setAttribute("src", "pickRandomImage");
-
-// document.querySelectorAll(".dice img2")[1].setAttribute("src", "pickRandomImage");
